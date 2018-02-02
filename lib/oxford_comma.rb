00813ed606e78bbcb1686ec36ab3087cade11e25
Alec German <alec_german@yahoo.com>
2018-02-02 02:56:06 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,8 @@
 def oxford_comma(array)
-  case array.length
-  when 0
+  if array.length == 0
     nil
-  when 1
+  elsif array.length == 1
     array[0]
-  when 2
+  elsif array.length == 3
     array.join(' and ')
-  when array.length > 2
-    array[0...array.length - 1].join(", ") + "and #{array.last}"
-  end
 end
